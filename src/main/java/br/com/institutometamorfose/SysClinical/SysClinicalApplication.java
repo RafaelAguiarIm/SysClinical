2a53e4dfc0cb38537f01d01edeed433bd6f85be7
Rafael Aguiar <rafael.aguiar.im@gmail.com>
2023-03-08 15:33:10 -0300

    first commit

---
+++ src/main/java/br/com/institutometamorfose/SysClinical/SysClinicalApplication.java
@@ -0,0 +1,13 @@
+package br.com.institutometamorfose.SysClinical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysClinicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysClinicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/institutometamorfose/SysClinical/SysClinicalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.institutometamorfose.SysClinical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysClinicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
